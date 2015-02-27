8d3041385377915884ec45c4f98b95b6f4aa74f4
Jefferson BONNAIRE <jefferson.bonnaire@gmail.com>
2015-02-27 04:37:54 +0000

    delete previous keys

--- includes/connection.php
+++ includes/connection.php
@@ -1,9 +1,9 @@
 <?php
 
-$PARAM_hote='db412966127.db.1and1.com'; // le chemin vers le serveur
-$PARAM_port='3306';
-$PARAM_nom_bd='db412966127'; // le nom de votre base de données
-$PARAM_utilisateur='dbo412966127'; // nom d'utilisateur pour se connecter
-$PARAM_mot_passe='972showtime972'; // mot de passe de l'utilisateur pour se connecter
+$PARAM_hote='localhost; // le chemin vers le serveur
+$PARAM_port='root';
+$PARAM_nom_bd='root'; // le nom de votre base de données
+$PARAM_utilisateur='root'; // nom d\'utilisateur pour se connecter
+$PARAM_mot_passe='root'; // mot de passe de l\'utilisateur pour se connecter
 
 ?>
